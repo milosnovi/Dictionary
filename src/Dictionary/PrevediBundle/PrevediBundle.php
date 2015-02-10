7ea574589db85d571c69a580aaec0855246ada2d
Milos Novicevic <milos@joiz.ch>
2015-02-10 08:54:50 +0100

    Start with API

---
+++ src/Dictionary/PrevediBundle/PrevediBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Dictionary\PrevediBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class PrevediBundle extends Bundle
+{
+}
---
+++ src/Dictionary/PrevediBundle/Tests/Controller/DefaultControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Dictionary\PrevediBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DefaultControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/hello/Fabien');
+
+        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+    }
+}
